25bbb1f83bfd6327d98df328a273db0f0ef22dae
Dorian Taylor <dorian.taylor.lists@gmail.com>
2012-12-06 14:05:32 -0800

    bit arithmetic for great justice

--- lib/Data/UUID/NCName.pm
+++ lib/Data/UUID/NCName.pm
@@ -34,8 +34,65 @@ our $VERSION = '0.01';
 
 =head1 DESCRIPTION
 
+The purpose of this module is to devise an alternative representation
+of the L<UUID|http://tools.ietf.org/html/rfc4122> which conforms to
+the constraints of various other identifiers such as NCName, and create an
+L<isomorphic|http://en.wikipedia.org/wiki/Isomorphism> mapping between
+them.
+
+=head1 RATIONALE & METHOD
+
 The UUID is a generic identifier which is large enough to be globally
-unique.
+unique. This makes it useful as a canonical name for data objects in
+distributed systems, especially those that cross administrative
+jurisdictions, such as the World-Wide Web. The L<representation>,
+however, of the UUID, precludes it from being used in many places
+where it would be useful to do so.
+
+In particular, there are grammars for many types of identifiers which
+must not begin with a digit. Others are case-insensitive, or
+prohibited from containing hyphens (present in both the standard
+notation and Base64URL), or indeed anything outside of
+C<^[A-Za-z_][0-9A-Za-z_]*$>.
+
+The hexadecimal notation of the UUID has a 5/8 chance of beginning
+with a digit, Base64 has a 5/32 chance, and Base32 has a 3/16
+chance. As such, the identifier must be modified in such a way as to
+guarantee beginning with an alphabetic letter (or underscore C<_>, but some
+grammars even prohibit that, so we omit it as well).
+
+While it is conceivable to simply add a padding character, there are a
+few considerations which make it more appealing to derive the initial
+character from the content of the UUID itself:
+
+=over 4
+
+=item *
+
+UUIDs are large (128-bit) identifiers as it is, and it is undesirable
+to add meaningless syntax to them if we can avoid doing so.
+
+=item *
+
+128 bits is an inconvenient number for aligning to both Base32 (130)
+and Base64 (132), though 120 divides cleanly into 5, 6 and 8.
+
+=item *
+
+The 13th quartet, or higher four bits of the
+C<time_hi_and_version_field> of the UUID is constant, as it indicates
+the UUID's version. If we encode this value using the scheme common to
+both Base64 and Base32, we get values between C<A> and C<P>, with the
+valid subset between C<B> and C<F>.
+
+=back
+
+B<Therefore:> extract the UUID's version quartet, shift all subsequent
+data 4 bits to the left, zero-pad to the octet, encode with either
+I<base64url> or I<base32>, truncate, and finally prepend the encoded
+version character. VoilE<agrave>, one token-safe UUID.
+
+=head1 APPLICATIONS
 
 =over 4
 
@@ -78,12 +135,35 @@ C<[A-Za-z][0-9A-Za-z]*>.
 
 =item Generated symbols
 
+=over 4
+
+There are only two hard things in computer science: cache
+invalidation, naming things [and off-by-one errors].
+
+E<mdash>Phil Karlton [extension of unknown origin]
+
+=back
+
+Suppose you wanted to create a L<literate
+programming|http://en.wikipedia.org/wiki/Literate_programming> system
+(I do). One of your (my) stipulations is that the symbols get defined
+in the I<prose>, rather than the I<code>. However, you (I) still want
+to be able to validate the code's syntax, and potentially even run the
+code, without having to commit to naming anything. You are (I am) also
+interested in creating a global map of classes, datatypes and code
+fragments, which can be operated on and tested in isolation, ported to
+other languages, or transplanted into the more conventional packages
+of programs, libraries and frameworks. The Base32 UUID NCName
+representation should be adequate for placeholder symbols in just
+about any programming language, save for those which do not permit
+identifiers as long as 26 characters (of which there are few).
+
 =back
 
 =head1 EXPORT
 
-No subroutines are exported by default. Be sure to include one of the
-following in your C<use> statement:
+No subroutines are exported by default. Be sure to include at least
+one of the following in your C<use> statement:
 
 =over 4
 
@@ -136,13 +216,18 @@ my $UUF = sprintf('%s-%s-%s-%s-%s', '%02x' x 4, ('%02x' x 2) x 3, '%02x' x 6);
 
 my %ENCODE = (
     32 => sub {
-        my $out = MIME::Base32::encode_rfc3548(shift);
+        my @in = unpack 'C*', shift;
+        $in[-1] >>= 1;
+        my $out = MIME::Base32::encode_rfc3548(pack 'C*', @in);
 
         # we want lower case because IT IS RUDE TO SHOUT
         lc substr($out, 0, 25);
     },
     64 => sub {
-        my $out = MIME::Base64::encode(shift);
+        my @in = unpack 'C*', shift;
+        #warn $in[-1] >> 2;
+        $in[-1] >>= 2;
+        my $out = MIME::Base64::encode(pack 'C*', @in);
         # note that the rfc4648 sequence ends in +/ or -_
         $out =~ tr!+/!-_!;
 
@@ -155,7 +240,10 @@ my %DECODE = (
         my $in = shift;
         $in = uc substr($in, 0, 25) . '0';
 
-        MIME::Base32::decode_rfc3548($in);
+        my @out = unpack 'C*', MIME::Base32::decode_rfc3548($in);
+        $out[-1] <<= 1;
+
+        pack 'C*', @out;
     },
     64 => sub {
         my $in = shift;
@@ -166,7 +254,11 @@ my %DECODE = (
 
         #warn unpack 'H*', MIME::Base64::decode($in);
 
-        MIME::Base64::decode($in);
+        my @out = unpack 'C*', MIME::Base64::decode($in);
+
+        $out[-1] <<= 2;
+
+        pack 'C*', @out;
     },
 );
 
@@ -189,7 +281,41 @@ sub _reassemble {
 
 sub _bin_uuid_to_pair {
     my $data = shift;
+    my @list = unpack 'N4', $data;
 
+    my $ver = ($list[1] & 0x0000f000) >> 12;
+    $list[1] = ($list[1] & 0xffff0000) |
+        (($list[1] & 0x00000fff) << 4) | ($list[2] >> 28);
+    $list[2] = ($list[2] & 0x0fffffff) << 4 | ($list[3] >> 28);
+    $list[3] <<= 4;
+
+    return $ver, pack 'N4', @list;
+}
+
+sub _pair_to_bin_uuid {
+    my ($ver, $data) = @_;
+
+    $ver &= 0xf;
+
+    my @list = unpack 'N4', $data;
+
+    $list[3] >>= 4;
+    $list[3] |= (($list[2] & 0xf) << 28);
+    $list[2] >>= 4;
+    $list[2] |= (($list[1] & 0xf) << 28);
+    $list[1] =
+        ($list[1] & 0xffff0000) | ($ver << 12) | (($list[1] >> 4) & 0xfff);
+
+    #warn unpack 'H*', pack 'N4', @list;
+
+    pack 'N4', @list;
+}
+
+sub _derp {
+#sub _bin_uuid_to_pair {
+    my $data = shift;
+
+    warn _foo($data);
     # this seems to do the right thing
     # warn unpack 'H*', $data;
 
@@ -206,7 +332,7 @@ sub _bin_uuid_to_pair {
     # ...pull out the version.
     my $ver = splice @list, 13, 1;
 
-    warn join('-', @list);
+    #warn join('-', @list);
 
     # rebuild as octets
     my $out = _reassemble(\@list);
@@ -218,7 +344,8 @@ sub _bin_uuid_to_pair {
     return ($ver, $out);
 }
 
-sub _pair_to_bin_uuid {
+sub _durr {
+#sub _pair_to_bin_uuid {
     my ($ver, $data) = @_;
 
     #warn unpack 'H*', $data;
@@ -479,11 +606,11 @@ Dorian Taylor, C<< <dorian at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-uuid-ncname at
-rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=UUID-NCName>.  I will
-be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+Please report any bugs or feature requests to C<bug-data-uuid-ncname
+at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-UUID-NCName>.  I
+will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 =head1 SUPPORT
 
@@ -497,19 +624,19 @@ You can also look for information at:
 
 =item * RT: CPAN's request tracker (report bugs here)
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=UUID-NCName>
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-UUID-NCName>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
-L<http://annocpan.org/dist/UUID-NCName>
+L<http://annocpan.org/dist/Data-UUID-NCName>
 
 =item * CPAN Ratings
 
-L<http://cpanratings.perl.org/d/UUID-NCName>
+L<http://cpanratings.perl.org/d/Data-UUID-NCName>
 
 =item * Search CPAN
 
-L<http://search.cpan.org/dist/UUID-NCName/>
+L<http://search.cpan.org/dist/Data-UUID-NCName/>
 
 =back
 
@@ -525,15 +652,22 @@ L<http://search.cpan.org/dist/UUID-NCName/>
 
 =item L<RFC 4648|http://tools.ietf.org/html/rfc4648>
 
-=item L<Namespaces in XML|http://www.w3.org/TR/xml-names/#NT-NCName> (NCName)
+=item L<Namespaces in XML|http://www.w3.org/TR/xml-names/#NT-NCName>
+(NCName)
 
-=item L<W3C XML Schema Definition Language (XSD) 1.1 Part 2: Datatypes|http://www.w3.org/TR/xmlschema11-2/#ID> (ID)
+=item L<W3C XML Schema Definition Language (XSD) 1.1 Part 2:
+Datatypes|http://www.w3.org/TR/xmlschema11-2/#ID> (ID)
 
-=item L<RDF/XML Syntax Specification (Revised)|http://www.w3.org/TR/rdf-syntax-grammar/#rdf-id>
+=item L<RDF/XML Syntax Specification
+(Revised)|http://www.w3.org/TR/rdf-syntax-grammar/#rdf-id>
 
 =item L<Turtle|http://www.w3.org/TR/turtle/#BNodes>
 
 =back
+
+This module lives under the C<Data::> namespace for the purpose of
+hygiene. It I<does not> depend on L<Data::UUID> or any other UUID
+modules.
 
 =head1 LICENSE AND COPYRIGHT
 
--- t/01-does-it-work.t
+++ t/01-does-it-work.t
@@ -38,30 +38,54 @@ BEGIN {
     }
 }
 
-plan tests => 4 * @UUIDS + 1;
+plan tests => 4 * @UUIDS + 6;
 
-use_ok('Data::UUID::NCName');
+use_ok('Data::UUID::NCName', ':all');
 
-#my $uu = 'e846399e-e8ab-4d47-bb97-26f870eec17b';
+# EXPECTED OUTPUTS
+
+my $z64 = to_ncname_64('00000000-0000-0000-0000-000000000000');
+my $z32 = to_ncname_32('00000000-0000-0000-0000-000000000000');
+
+is($z64, 'AAAAAAAAAAAAAAAAAAAAAA',     'Null64 UUID OK');
+is($z32, 'Aaaaaaaaaaaaaaaaaaaaaaaaaa', 'Null32 UUID OK');
+
+my $f64 = to_ncname_64('ffffffff-ffff-ffff-ffff-ffffffffffff');
+my $f32 = to_ncname_32('ffffffff-ffff-ffff-ffff-ffffffffffff');
+
+diag('E' . MIME::Base64::encode_base64url(pack 'C*', (255) x 15, 15 << 2));
+
+is($f64, 'P____________________P',     'FF64 UUID OK');
+is($f32, 'P777777777777777777777777p', 'FF32 UUID OK');
+
+my $f064 = to_ncname_64('ffffffff-ffff-4fff-ffff-fffffffffff0');
+my $z064 = to_ncname_64('00000000-0000-4000-0000-00000000000f');
+
+#my $lint = substr
+#    MIME::Base64::encode_base64url(pack 'C*', (0) x 15, 0xf << 2), 0, 21;
+
+#diag(MIME::Base64::encode_base64url(pack 'C*', 0b00001111));
+#diag(MIME::Base64::encode_base64url(pack 'C*', 0b00111100));
+#diag(MIME::Base64::encode_base64url(pack 'C*', 0b11110000));
+
+#diag(unpack 'H*', MIME::Base64::decode_base64url('8PA'));
+
+# D    w
+# 0x03 0x30
+
+#diag("E$lint");
+
+is($f064, 'E____________________A', 'F064 ends with A');
+
+#diag($f064);
+#diag($z064);
+
+# FUZZ TESTING
 
 for my $uu (@UUIDS) {
     #diag($uu);
     mooltipass($uu);
 }
-
-diag(Data::UUID::NCName::to_ncname('00000000-0000-0000-0000-000000000000'));
-
-my $wtf = Data::UUID::NCName::to_ncname('f0000000-0000-4000-0000-00000000000f');
-diag($wtf);
-diag(Data::UUID::NCName::from_ncname($wtf));
-
-my $ff = Data::UUID::NCName::to_ncname('ffffffff-ffff-4fff-ffff-fffffffffff1');
-
-diag($ff);
-
-diag(Data::UUID::NCName::from_ncname($ff));
-
-diag(Data::UUID::NCName::from_ncname('E_wAAAAAAAAAAAAAAAAAAP'));
 
 sub mooltipass {
     my $uuid = shift;
@@ -82,3 +106,6 @@ sub mooltipass {
     is($uu32, $uuid, 'Base32 content matches original UUID');
 
 }
+
+diag(to_ncname('00000000-0000-4000-0000-00000000000f'));
+diag(from_ncname('EAAAAAAAAAAAAAAAAAAAAP'));
